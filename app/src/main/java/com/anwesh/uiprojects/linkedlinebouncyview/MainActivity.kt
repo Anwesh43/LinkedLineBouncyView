3a5c725e077822d90d4c8031b8f9b332e226baf7
Anwesh43 <anweshthecool0@gmail.com>
2019-11-04 10:30:03 +0530

    adding app module

---
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinebouncyview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.anwesh.uiprojects.linkedlinebouncyview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
