94ecacb33328fa3e83eaa086c8e9dbaf82e25742
Anwesh43 <anweshthecool0@gmail.com>
2019-11-04 11:07:40 +0530

    adding Canvas extension methods

--- linebouncyview/src/main/java/com/anwesh/uiprojects/linebouncyview/LineBouncyView.kt
+++ linebouncyview/src/main/java/com/anwesh/uiprojects/linebouncyview/LineBouncyView.kt
@@ -15,7 +15,7 @@ import android.content.Context
 val nodes : Int = 5
 val scGap : Float = 0.02f
 val strokeFactor : Int = 90
-val sizeFactor : Float = 2.9f
+val sizeFactor : Float = 8f
 val delay : Long = 20
 val foreColor : Int = Color.parseColor("#3F51B5")
 val backColor : Int = Color.parseColor("#BDBDBD")
@@ -23,3 +23,29 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawLineBouncy(gap : Float, w : Float, size : Float, scale : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val sf : Float = sc2.sinify()
+    val x : Float = w * sc2
+    val y : Float = -gap * sf
+    drawCircle(x, y, size, paint)
+    drawLine(0f, 0f, size * sc1, 0f, paint)
+}
+
+fun Canvas.drawLBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(0f, gap * (i + 1))
+    drawLineBouncy(gap, w, size, scale, paint)
+    restore()
+}
+
